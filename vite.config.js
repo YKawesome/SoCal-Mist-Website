e65b90c2a027cb2f1e4ff519141d4176d7e86dce
Yousef <theyousefkhan@gmail.com>
2025-02-24 00:03:46 -0800

    --amend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import viteStaticCopy from "vite-plugin-static-copy";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
