611da336ab34e919b297ba83303c8cec7d68c3a9
Yousef <theyousefkhan@gmail.com>
2025-04-17 19:32:40 -0700

    --amend

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,6 +1,6 @@
 function PageHeader({ title }) {
   return (
-    <div className="bg-primary text-primary-content h-[12vh] flex items-center justify-center">
+    <div className="bg-primary text-primary-content h-[15vh] flex items-center justify-center">
       <h1 className="sm:text-6xl text-4xl font-bold text-center">{title}</h1>
     </div>
   );
