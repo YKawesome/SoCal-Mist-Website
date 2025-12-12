a372747b516284777629c8d3ccde36cd94a97394
Yousef <theyousefkhan@gmail.com>
2025-12-11 23:23:55 -0800

    eboard updates

--- src/data/boardData.js
+++ src/data/boardData.js
@@ -12,12 +12,12 @@ export const boardData = [
     committee: "Marketing",
     members: [
       {
-        role: "Webmaster",
-        name: "Yousef Khan",
+        role: "Chair",
+        name: "Saahim Siddiqui",
       },
       {
-        role: "Communications Coordinator",
-        name: "Yasmine Ibrahim",
+        role: "Webmaster",
+        name: "Yousef Khan",
       },
     ],
   },
@@ -26,7 +26,7 @@ export const boardData = [
     members: [
       {
         role: "Chair",
-        name: "Sawsan Dibseh",
+        name: "Samir Yousef",
       },
     ],
   },
@@ -35,29 +35,21 @@ export const boardData = [
     members: [
       {
         role: "Chair",
-        name: "Mahnoor Saleem",
+        name: "Yasmine Ibrahim",
       },
       {
-        role: "Spirits Coordinator",
-        name: "Zeinab Arafa",
-      },
-      {
-        role: "MISTChats Coordinator",
-        name: "Zoya Rana",
-      },
+        role: "Organizer",
+        name: "Zoya Rana"
+      }
     ],
   },
   {
     committee: "Outreach and Registration",
     members: [
-      {
-        role: "Chair",
-        name: "Araam Abutaha",
-      },
-      {
-        role: "O/R Coordinator",
-        name: "Ali Khaja",
-      },
+      // {
+      //   role: "Chair",
+      //   name: "Araam Abutaha",
+      // },
     ],
   },
   {
@@ -65,27 +57,11 @@ export const boardData = [
     members: [
       {
         role: "Chair",
-        name: "Lujain Almahdi",
-      },
-      {
-        role: "Knowledge/Quran Head",
-        name: "Boushra El Habbal",
-      },
-      {
-        role: "Writing/Oratory Head",
-        name: "Samir Yousef",
-      },
-      {
-        role: "Brackets Head",
-        name: "Yosuf Mayar",
-      },
-      {
-        role: "Brackets Head",
-        name: "Yazan Moubayed",
-      },
-      {
-        role: "Group Projects Head",
         name: "Summer Kurdi",
+      },
+      {
+        role: "Organizer",
+        name: "Yazan Moubayed",
       },
     ],
   },
